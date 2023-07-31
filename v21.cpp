a91ed0d8246c52f66885625b5b9fa95036be2b05
arthunix <thursilverio@gmail.com>
2023-07-31 01:24:24 -0300

    erro nao abaixa bixo

--- v21.cpp
+++ v21.cpp
@@ -8,89 +8,79 @@
 #include "uart.hpp"
 #include "config.hpp"
 
+//#define DEBUG_TO_A_FILE
+
 void V21_RX::demodulate(const float *in_analog_samples, unsigned int n)
 {
     unsigned int digital_samples[n];
 
+#ifdef DEBUG_TO_A_FILE
     FILE *dc   = fopen("../dc.raw", "wb");
     FILE *f_dc = fopen("../f_dc.raw", "wb");
     FILE *f_v0 = fopen("../v0.raw", "wb");
     FILE *f_v1 = fopen("../v1.raw", "wb");
+#endif
 
-    static enum class State {
-        NO_STATE,
-        BEARER_NOT_DETECT,
-        BEARER_DETECT
-    } state = State::BEARER_NOT_DETECT;
+    const unsigned int  R = 300;
+    const unsigned int fs = 48000;
+    const unsigned int  L = std::floor( fs / R );
+    const float         T = 1/fs;
+    const float omega0=2*M_PI*1850;
+    const float omega1=2*M_PI*1650;
+    const float r = 0.99;
 
-    unsigned int  R = 300;
-    unsigned int fs = 48000;
-    float         T = 1/fs;
-    unsigned int  L = std::floor( fs / R );
+    this->omega_mark = omega0;
+    this->omega_space = omega1;
 
-    int bit_num = 0;
-
-    float omega0=2*M_PI*1850;
-    float omega1=2*M_PI*1650;
-
-    omega_mark = omega0;
-    omega_space = omega1;
-
-    float butter_a[3] = { 1.0        , -1.94447766 , 0.94597794 };
-    float butter_b[3] = { 0.00037507 ,  0.00075014 , 0.00037507 };
-
-    float v0i, v0r, v1i, v1r, filtered_decision, decision;
-    float r = 0.99;
-
-    float v0, v1;
+    const float butter_a[3] = { 1.0        , -1.94447766 , 0.94597794 };
+    const float butter_b[3] = { 0.00037507 ,  0.00075014 , 0.00037507 };
 
     // substitua o loop abaixo, que gera um sinal UART constantemente ocioso, pelo seu código
     for (int i = 0; i < n; i++) {
-        int mod = i % (SAMPLES_PER_SYMBOL + 1);
-        ring_buffer[mod] = in_analog_samples[i];
+        const float s = in_analog_samples[i];
+        ring_buffer[ring_buffer_it] = s;
+        ring_buffer_it = (ring_buffer_it + 1) % (L + 1);
+        const float sL = ring_buffer[ring_buffer_it];
 
-        v0r = ring_buffer[mod] - pow(r,L) * cos(omega0*L*T)*ring_buffer[mod] + r*cos(omega0*T)*v0r_prev - r*sin(omega0*T)*v1i_prev;
-        v0i = -1*pow(r,L)*sin(omega0*L*T)*ring_buffer[mod] + r*cos(omega0*T)*v0i_prev + r*sin(omega0*T)*v1r_prev;
-        v1r = ring_buffer[mod] - pow(r,L) * cos(omega1*L*T)*ring_buffer[mod] + r*cos(omega1*T)*v0r_prev - r*sin(omega1*T)*v1i_prev;
-        v1i = -1*pow(r,L)*sin(omega1*L*T)*ring_buffer[mod] + r*cos(omega1*T)*v0i_prev + r*sin(omega1*T)*v1r_prev;
+        const float v0r = s - pow(r,L) * cos(omega0*L*T)*s + r*cos(omega0*T)*v0r_prev - r*sin(omega0*T)*v1i_prev;
+        const float v0i = -1*pow(r,L)*sin(omega0*L*T)*s + r*cos(omega0*T)*v0i_prev + r*sin(omega0*T)*v1r_prev;
+        const float v1r = s - pow(r,L) * cos(omega1*L*T)*s + r*cos(omega1*T)*v0r_prev - r*sin(omega1*T)*v1i_prev;
+        const float v1i = -1*pow(r,L)*sin(omega1*L*T)*s + r*cos(omega1*T)*v0i_prev + r*sin(omega1*T)*v1r_prev;
+
+        this->v0i_prev = v0i; this->v0r_prev = v0r; this->v1i_prev = v1i; this->v1r_prev = v1r;
+
+        const float v0 = v0r*v0r + v0i*v0i; const float v1 = v1r*v1r + v1i*v1i;
 
         // define-se um sinal de decisão, que se for positivo indica que devemos gerar nível lógico alto
-        decision = v1r*v1r + v1i*v1i - v0r*v0r - v0i*v0i;
-        v0 = v0r*v0r + v0i*v0i;
-        v1 = v1r*v1r + v1i*v1i;
+        const float decision = v1 - v0;
 
         // Passa-se o sinal por um filtro passa-baixas com corte em 300 Hz.
-        filtered_decision = butter_b[0] * decision + butter_b[1] * decision_prev + butter_b[2] * decision_prev_prev - butter_a[1] * filtered_decision_prev - butter_a[2] * filtered_decision_prev_prev;
+        const float filtered_decision = butter_b[0] * decision + butter_b[1] * decision_prev + butter_b[2] * decision_prev_prev - butter_a[1] * filtered_decision_prev - butter_a[2] * filtered_decision_prev_prev;
 
+        this->filtered_decision_prev_prev = filtered_decision_prev; this->decision_prev_prev = decision_prev;
+
+#ifdef DEBUG_TO_A_FILE
         fwrite(&v0, 1, sizeof(float), f_v0);
         fwrite(&v1, 1, sizeof(float), f_v1);
+#endif
 
         //Estado portadora detectada 
-        if(filtered_decision > 120.)
+        if(std::fabs(filtered_decision) > 60.)
         {
-            bit_num = 50;
-            state = State::BEARER_NOT_DETECT;  
+            counter = 50;
         }
-        else
+        else if(counter > 0 && std::fabs(filtered_decision) < 50.)
         {
-            bit_num--;
-            if(bit_num == 0.)
-                state = State::BEARER_DETECT;
+            counter--;
         }
 
-        if(state == State::BEARER_DETECT)
+        if(counter > 0)
+            digital_samples[i] =  filtered_decision >= 0.;
+        else
             digital_samples[i] = 1;
-        else if(state == State::BEARER_NOT_DETECT)
-            digital_samples[i] = 0;
-    
-        v0i_prev = v0i;
-        v0r_prev = v0r;
-        v1i_prev = v1i;
-        v1r_prev = v1r;
-        filtered_decision_prev_prev = filtered_decision_prev;
-        decision_prev_prev = decision_prev;
     }
 
+#ifdef DEBUG_TO_A_FILE
     fwrite(&decision, n, sizeof(float), dc);
     fwrite(&filtered_decision, n, sizeof(float), f_dc);
 
@@ -98,6 +88,7 @@ void V21_RX::demodulate(const float *in_analog_samples, unsigned int n)
     fclose(f_dc);
     fclose(f_v0);
     fclose(f_v1);
+#endif
 
     get_digital_samples(digital_samples, n);
 }
--- v21.hpp
+++ v21.hpp
@@ -12,9 +12,13 @@ public:
     void demodulate(const float *in_analog_samples, unsigned int n);
 private:
     float omega_mark, omega_space;
-    float v0i_prev, v0r_prev, v1i_prev, v1r_prev, filtered_decision_prev, filtered_decision_prev_prev, decision_prev, decision_prev_prev;
+    float v0i_prev, v0r_prev, v1i_prev, v1r_prev;
+    float decision_prev, decision_prev_prev;
+    float filtered_decision_prev, filtered_decision_prev_prev;
     std::function<void(const unsigned int *, unsigned int)> get_digital_samples;
-    float ring_buffer[SAMPLES_PER_SYMBOL + 1] = {};
+    float ring_buffer[SAMPLES_PER_SYMBOL] = {};
+    unsigned int ring_buffer_it = 0;
+    unsigned int counter = 0;
 };
 
 class V21_TX
