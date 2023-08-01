88039853745f56908dc88e128cd9579d9a37f1df
Thiago Martins <31443982+thiago0003@users.noreply.github.com>
2023-08-01 16:51:53 -0300

    Update v21.cpp

--- v21.cpp
+++ v21.cpp
@@ -21,19 +21,24 @@ void V21_RX::demodulate(const float *in_analog_samples, unsigned int n)
     FILE *f_v1 = fopen("../v1.raw", "wb");
 #endif
 
-    const unsigned int  R = 300;
-    const unsigned int fs = 48000;
-    const unsigned int  L = std::floor( fs / R );
-    const float         T = 1/fs;
-    const float omega0=2*M_PI*1850;
-    const float omega1=2*M_PI*1650;
-    const float r = 0.99;
+    constexpr int  L = SAMPLES_PER_SYMBOL;
+    constexpr float T = SAMPLING_PERIOD;
+    
+    constexpr float r = 0.99;
 
-    this->omega_mark = omega0;
-    this->omega_space = omega1;
+    const float rL = pow(r, L);
+    const float cos0LT = cos(omega_space*L*T);
+    const float sin0LT = sin(omega_space*L*T);
+    const float cos0T = cos(omega_space*T);
+    const float sin0T = sin(omega_space*T);
 
-    const float butter_a[3] = { 1.0        , -1.94447766 , 0.94597794 };
-    const float butter_b[3] = { 0.00037507 ,  0.00075014 , 0.00037507 };
+    const float cos1LT = cos(omega_mark*L*T);
+    const float sin1LT = sin(omega_mark*L*T);
+    const float cos1T = cos(omega_mark*T);
+    const float sin1T = sin(omega_mark*T);
+
+    //const float butter_a[3] = { 1.0        , -1.94447766 , 0.94597794 };
+    //const float butter_b[3] = { 0.00037507 ,  0.00075014 , 0.00037507 };
 
     // substitua o loop abaixo, que gera um sinal UART constantemente ocioso, pelo seu código
     for (int i = 0; i < n; i++) {
@@ -42,22 +47,27 @@ void V21_RX::demodulate(const float *in_analog_samples, unsigned int n)
         ring_buffer_it = (ring_buffer_it + 1) % (L + 1);
         const float sL = ring_buffer[ring_buffer_it];
 
-        const float v0r = s - pow(r,L) * cos(omega0*L*T)*s + r*cos(omega0*T)*v0r_prev - r*sin(omega0*T)*v1i_prev;
-        const float v0i = -1*pow(r,L)*sin(omega0*L*T)*s + r*cos(omega0*T)*v0i_prev + r*sin(omega0*T)*v1r_prev;
-        const float v1r = s - pow(r,L) * cos(omega1*L*T)*s + r*cos(omega1*T)*v0r_prev - r*sin(omega1*T)*v1i_prev;
-        const float v1i = -1*pow(r,L)*sin(omega1*L*T)*s + r*cos(omega1*T)*v0i_prev + r*sin(omega1*T)*v1r_prev;
+        const float v0r = s - rL*cos0LT*sL + r*cos0T*v0r_prev - r*sin0T*v1i_prev;
+        const float v0i = -rL*cos0LT*s + r*cos0T*v0r_prev + r*sin0T*v1r_prev;
+        const float v1r = s - rL*cos1LT*s + r*cos1T*v0r_prev - r*sin1T*v1i_prev;
+        const float v1i = -rL*sin1LT*s + r*cos1T*v0i_prev + r*sin1T*v1r_prev;
 
-        this->v0i_prev = v0i; this->v0r_prev = v0r; this->v1i_prev = v1i; this->v1r_prev = v1r;
+        v0i_prev = v0i; v0r_prev = v0r; v1i_prev = v1i; v1r_prev = v1r;
 
-        const float v0 = v0r*v0r + v0i*v0i; const float v1 = v1r*v1r + v1i*v1i;
+        const float v0 = v0r*v0r + v0i*v0i;
+        const float v1 = v1r*v1r + v1i*v1i;
 
         // define-se um sinal de decisão, que se for positivo indica que devemos gerar nível lógico alto
         const float decision = v1 - v0;
 
         // Passa-se o sinal por um filtro passa-baixas com corte em 300 Hz.
-        const float filtered_decision = butter_b[0] * decision + butter_b[1] * decision_prev + butter_b[2] * decision_prev_prev - butter_a[1] * filtered_decision_prev - butter_a[2] * filtered_decision_prev_prev;
+        const float filtered_decision = 0.00037507* decision + 0.00075014*decision_prev + 0.00037507*decision_prev_prev + 1.94447766 * filtered_decision_prev - 0.94597794 * filtered_decision_prev_prev;
 
-        this->filtered_decision_prev_prev = filtered_decision_prev; this->decision_prev_prev = decision_prev;
+        filtered_decision_prev_prev = filtered_decision_prev; 
+        filtered_decision_prev = filtered_decision;
+
+        decision_prev_prev = decision_prev;
+        decision_prev = decision;
 
 #ifdef DEBUG_TO_A_FILE
         fwrite(&v0, 1, sizeof(float), f_v0);
@@ -71,7 +81,7 @@ void V21_RX::demodulate(const float *in_analog_samples, unsigned int n)
         }
         else if(counter > 0 && std::fabs(filtered_decision) < 50.)
         {
-            counter--;
+            --counter;
         }
 
         if(counter > 0)
